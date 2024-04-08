95c33591cd0da79c3158eceaad45574469a9c5e6
Nick Volynkin <nick.volynkin@gmail.com>
2024-04-08 19:19:09 +0300

    update for new syntax

--- playwright.config.js
+++ playwright.config.js
@@ -7,11 +7,14 @@ const config = {
     ['list'],
     ['playwright-qase-reporter',
       {
+        mode: 'testops',
+        fallback: 'report',
         testops: {
           project: 'PWR',
           uploadAttachments: true,
         },
         run: {
+          title: 'example test run',
           complete: true
         },
         logging: true,
--- tests/google.test.js
+++ tests/google.test.js
@@ -1,21 +1,37 @@
 // tests/google.test.js
 import {expect, test} from "@playwright/test";
-import {qase} from 'playwright-qase-reporter/playwright';
-// import {qase} from 'playwright-qase-reporter/dist/playwright';
-
-// const {test, expect} = require('@playwright/test');
+import {qase} from 'playwright-qase-reporter';
 
 test.describe("Example tests", async () => {
-  test(qase('1', 'Google homepage has the correct title'), async ({page}) => {
-  // test( 'Google homepage has the correct title', async ({page}) => {
-  //   qase.id(1)
-  //   qase.fields(
-  //     {
-  //       severity: 'high',
-  //       priority: 'medium'
-  //     }
-  //   )
+  test('Google homepage has the correct title', async ({page}) => {
+    qase.id(1)
+    qase.title('Google homepage has the correct title')
+    qase.fields(
+      {
+        severity: 'high',
+        priority: 'medium'
+      }
+    )
     await page.goto('https://google.com');
     await expect(page).toHaveTitle(/Google/);
   });
+
+
+  test(qase(2, 'Google homepage — with stfeps'), async ({page}) => {
+    await test.step('Step 1', async () => {
+      await page.goto('https://google.com');
+    });
+    await test.step('Step 2', async () => {
+      await expect(page).toHaveTitle(/Google/);
+    });
+  });
+
+  // const people = ['Alice', 'Bob'];
+  // for (const name of people) {
+  //   test(`testing with ${name}`, async () => {
+  //     qase.id(3)
+  //     qase.parameters({'person': name})
+  //   });
+  // }
+
 })
